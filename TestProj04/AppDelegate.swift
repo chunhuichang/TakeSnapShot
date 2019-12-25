31c742211d626ec863ae65169e5475c69dace775
張淳惠 <jillchang@cathaylife.com.tw>
2019-12-25 17:37:20 +0800

    feat:add snap shot and share

--- TestProj04/AppDelegate.swift
+++ TestProj04/AppDelegate.swift
@@ -10,28 +10,49 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("didFinishLaunchingWithOptions")
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        print("applicationWillTerminate")
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        print("applicationDidBecomeActive")
     }
-
-
+    func applicationWillResignActive(_ application: UIApplication) {
+        print("applicationWillResignActive")
+    }
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        print("applicationDidEnterBackground")
+    }
+    func applicationDidFinishLaunching(_ application: UIApplication) {
+        print("applicationDidFinishLaunching")
+    }
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        print("applicationWillEnterForeground")
+    }
+    func applicationSignificantTimeChange(_ application: UIApplication) {
+        print("applicationSignificantTimeChange")
+    }
+    func applicationDidReceiveMemoryWarning(_ application: UIApplication) {
+        print("applicationDidReceiveMemoryWarning")
+    }
+    func applicationProtectedDataDidBecomeAvailable(_ application: UIApplication) {
+        print("applicationProtectedDataDidBecomeAvailable")
+    }
+    func applicationShouldRequestHealthAuthorization(_ application: UIApplication) {
+        print("applicationShouldRequestHealthAuthorization")
+    }
+    func applicationProtectedDataWillBecomeUnavailable(_ application: UIApplication) {
+        print("applicationProtectedDataWillBecomeUnavailable")
+    }
+    
 }
 
--- TestProj04/ViewController.swift
+++ TestProj04/ViewController.swift
@@ -10,11 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func shareClick(_ sender: UIButton) {
+        
+        //snap shot
+        let screenshot = self.takeSnapShot(currentView: self.view, hideViews: [self.label1])
+        
+        //share photo
+        let activityViewController = UIActivityViewController(activityItems: [screenshot], applicationActivities: nil)
+        if let popoverController = activityViewController.popoverPresentationController {
+            popoverController.sourceRect = sender.convert(sender.bounds, to: self.view)
+            popoverController.sourceView = self.view
+            popoverController.permittedArrowDirections = .left
+        }
 
+        self.present(activityViewController, animated: true, completion: nil)
+        
+    }
+    
+    //截圖
+    func takeSnapShot(currentView: UIView , hideViews: [UIView]?) -> UIImage {
+        // excluded view
+        if let hideViews = hideViews {
+            for hideView in hideViews {
+                hideView.isHidden = true
+            }
+        }
+
+        // begin
+        UIGraphicsBeginImageContextWithOptions(currentView.frame.size, false, 0.0)
+        
+        // draw view in that context.
+        currentView.drawHierarchy(in: currentView.bounds, afterScreenUpdates: true)
+        
+        // get iamge
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        if let hideViews = hideViews {
+            for hideView in hideViews {
+                hideView.isHidden = false
+            }
+        }
+        
+        if (image != nil){
+            return image!
+        }
+        
+        return UIImage()
+    }
 }
 
